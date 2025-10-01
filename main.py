e6cd0dd6d641871f2f75c50205294e8f0d4bdab8
Ashan Sanjana <160347827+Ashansanjana@users.noreply.github.com>
2025-10-01 17:11:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from utils.video_utils import read_video, save_video
 from trackers.tracker import Tracker
 from team_assigner.team_assigner import TeamAssigner
+from player_ball_assignment.player_ball_assigner import PlayerBallAssigner
+import numpy as np
 
 def main():
     # Read Video
@@ -8,13 +10,16 @@ def main():
     
     # Initialize Tracker
     tracker = Tracker('models/best.pt')
-
+ 
     # Get object tracks (load from stub if available)
     tracks = tracker.get_object_tracks(
         video_frames,
         read_from_stub=True,
         stub_path='stubs/track_stubs.pkl'
     )
+
+    # Interpolate Ball Positions
+    tracks["ball"] = tracker.interpolate_ball_positions(tracks["ball"])
 
     # Assign Player Teams
     team_assigner = TeamAssigner()
@@ -28,10 +33,23 @@ def main():
                                                  player_id)
             tracks['players'][frame_num][player_id]['team'] = team 
             tracks['players'][frame_num][player_id]['team_color'] = team_assigner.team_colors[team]
-            
+
+    # Assign Ball Aquisition
+    player_assigner =PlayerBallAssigner()
+    team_ball_control= []
+    for frame_num, player_track in enumerate(tracks['players']):
+        ball_bbox = tracks['ball'][frame_num][1]['bbox']
+        assigned_player = player_assigner.assign_ball_to_player(player_track, ball_bbox)
+
+        if assigned_player != -1:
+            tracks['players'][frame_num][assigned_player]['has_ball'] = True
+            team_ball_control.append(tracks['players'][frame_num][assigned_player]['team'])
+        else:
+            team_ball_control.append(team_ball_control[-1])
+    team_ball_control= np.array(team_ball_control)
 
     # Draw annotations (without ball control for now)
-    output_video_frames = tracker.draw_annotations(video_frames, tracks)
+    output_video_frames = tracker.draw_annotations(video_frames, tracks,team_ball_control)
 
     # Save video
     save_video(output_video_frames, 'output_videos/output_video.avi')
